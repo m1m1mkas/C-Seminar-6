13964c72014ef9f4a858557a48ab767e1ec5fea0
m1m1m <m1m1mkas@mail.ru>
2022-09-12 11:51:44 +0300

    Seminar 6

---
+++ Task_3/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine();
+
+int size = new Random().Next(10, 50);
+int min = 1;
+int max = 100;
+Console.WriteLine($"Количество элементов массива: {size}");
+
+int[] FillArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+int[] GetPairProduct(int[] data)
+{
+    int[] product = new int[data.Length / 2];
+    int last = data.Length - 1;
+    for (int i = 0; i < data.Length / 2; i++)
+    {
+        product[i] = data[i] * data[last];
+        last = last - 1;
+    }
+    return product;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = FillArray(size, min, max);
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine("Сумма произведений пар чисел:");
+PrintArray(GetPairProduct(array));
